ed001162533bb8fdc8b3aeb9a721f7711e3220ae
tjkendev <jake-basu@hotmail.co.jp>
2019-06-22 17:36:22 +0900

    run.shの直し

--- yaketake08/run.sh
+++ yaketake08/run.sh
@@ -7,5 +7,15 @@ echo $outputs
 for input_file in `find $inputs/*.desc`; do
   output_file=`echo ${outputs}/$(basename $input_file) | sed -e 's/.desc/.sol/g'`
   echo "< ${input_file} > ${output_file}"
+  if [ -f ${output_file} ]; then
+    # skip if the output file already exists
+    echo "skip"
+    continue
+  fi
   python3 ./programs/simple-dfs.py ${input_file} > ${output_file}
+  if [ $? != 0 ]; then
+    # delete file if fails to execute the program
+    echo "deleted"
+    rm ${output_file}
+  fi
 done
