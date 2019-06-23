2c3f4515c26d433f5d734f55d3320823b168851c
tjkendev <jake-basu@hotmail.co.jp>
2019-06-23 21:31:34 +0900

    マージするやつ

--- yaketake08/programs/shortcut-simple-dfs.py
+++ yaketake08/programs/shortcut-simple-dfs.py
@@ -20,13 +20,13 @@ for i in range(1, Y-1):
             MP1[i][j] = 1
 
 cnt = 0
-W = [[0]*(X+1) for i in range(Y+1)]
+W = [[1]*(X+1) for i in range(Y+1)]
 for i in range(Y):
     for j in range(X):
         if MP1[i][j] != 1:
             cnt += 1
-        if MP[i][j] == 1:
-            W[i][j] = 1
+        if MP[i][j] == 0:
+            W[i][j] = 0
 
 dd = ((0, -1, 0), (1, 0, -1), (2, 1, 0), (3, 0, 1))
 C = "ASDW"
@@ -54,6 +54,12 @@ def dfs(x, y):
         res.append((x, y, 1))
     else:
         res.append((x, y, 0))
+    # だいたい改善する (悪化する場合もある)
+    # 部屋の左上に塗られていないマスを放置してしまう挙動をするため、これに対応するもの
+    if MP[y][x-1] == 1 and MP[y+1][x] == 0 and W[y+1][x] == 0 and MP[y+2][x] == 1 and MP[y+2][x+1] == 1 and MP[y+1][x-1] == 1:
+        res.append((x, y+1, 1))
+        res.append((x, y, 0))
+        W[y+1][x] = 1
     W[y][x] = W[y][x+1] = W[y-1][x+1] = W[y+1][x+1] = 1
     cnt -= 1
     for i in orders[y][x]:
