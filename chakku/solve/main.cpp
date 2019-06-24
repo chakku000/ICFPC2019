1068d30ac222571bed64970ad0e0c0d50fbed3b5
chakku <chakkurai0724@outlook.jp>
2019-06-24 13:42:56 +0900

    submit2

--- chakku/solve/main.cpp
+++ chakku/solve/main.cpp
@@ -1,81 +1,8 @@
 #include <bits/stdc++.h>
+#include "poi.hpp"
 using namespace std;
-//template{{{
-#define rep(i,n) for(int i=0;i<(int)(n);i++)
-#define all(v) (v).begin(),(v).end()
-template<typename T> ostream& operator<<(ostream& os,const vector<T>& vec){ os << "["; for(const auto& v : vec){ os << v << ","; } os << "]"; return os; }
-template<typename T,typename U> ostream& operator<<(ostream& os, const map<T,U>& mp){ os << "{"; for(auto& p : mp){ os << p << ","; } os << "}"; return os; }
-template<typename T,typename U> ostream& operator<<(ostream& os,const pair<T,U>& p){ os << "(" << p.first << ","<< p.second <<")"; return os; }
-template<typename T> ostream& operator<<(ostream& os,const set<T>& st){ os<<"{"; for(T v:st) os<<v<<","; os <<"}"; return os; }
-typedef pair<int,int> pii;/*}}}*/
-const int dx[]={1,0,-1,0} ,dy[] = {0,1,0,-1};
-const char word[] = {'D','W','A','S'};
-constexpr int INF = numeric_limits<int>::max()/2;
-using P = pair<int,pii>;
-/*}}}*/
-
-int offset_x,offset_y;
-int w,h;
-int sx,sy;
-vector<vector<char>> table;
-
-constexpr int BEAM = 8;
-constexpr int DEPTH = 30;
-
-char direction(const pii& p1,const pii& p2){
-    for(int i=0;i<4;i++){
-        if(p1.first + dx[i] == p2.first and p1.second + dy[i] == p2.second){
-            return word[i];
-        }
-    }
-    //assert(false);
-    cout << p1 << " " << p2 << endl;
-    //exit(0);
-    return 'X';
-}
 
 
-struct Action{
-    char action;
-    int x,y;
-    Action(char c): action(c){}
-    Action(char c,int xx,int yy) : action(c) , x(xx) , y(yy){}
-};
-ostream& operator<<(ostream& os, const Action& action){
-    os << "Action(" << action.action<< ")";
-    return os;
-}
-
-struct State{/*{{{*/
-public:
-    int x,y;
-    int value;
-    int wheel=0;
-    /* int drill=0; */
-    int arm=0;
-    int wheelturn=0;
-    /* int drillturn=0; */
-    vector<vector<bool>> visit;
-    vector<pii> arms;
-    vector<Action> actions;
-
-    int eval()const{
-        return value + actions.size();
-    }
-
-    bool operator<(const State& st)const{
-        return eval() < st.eval();
-    }
-    void init(){
-        visit.assign(w,vector<bool>(h,false));
-    }
-};/*}}}*/
-
-void print(State& s){
-    cerr << "(" << s.x << "," << s.y << ") , " << "value = " << s.value << " actions = " << s.actions << endl;
-}
-
-bool valid_index(int x,int y){ return !(x<0 or y<0 or x>=w or y>=h); }
 
 
 void update_move(State& state, int dx,int dy){/*{{{*/
@@ -89,65 +16,53 @@ void update_move(State& state, int dx,int dy){/*{{{*/
         return;
     }
 
-
     state.x += dx;
     state.y += dy;
 
     int cnt = 0; // 到達になる場所の個数
 
+    state.touch();
     // bodyの通過
-    if(!state.visit[state.x][state.y]) cnt++;
-    if(table[state.x][state.y] == 'B') state.arm++;
-    if(table[state.x][state.y] == 'F') state.wheel++;
-    state.visit[state.x][state.y] = true;
+    //if(!state.visit[state.x][state.y]) cnt++;
+    //if(table[state.x][state.y] == 'B') state.arm++;
+    //if(table[state.x][state.y] == 'F') state.wheel++;
+    //state.visit[state.x][state.y] = true;
 
-    // 手が届く範囲
-    for(const auto& p : state.arms){
-        int x = state.x + p.first;
-        int y = state.y + p.second;
-        if(x<0 or y<0 or x>=w or y>=h) continue;
-        if(table[x][y]!='#' and !state.visit[x][y]) cnt++;
-        state.visit[x][y] = true;
-    }
+    //// 手が届く範囲
+    //for(const auto& p : state.arms){
+    //    int x = state.x + p.first;
+    //    int y = state.y + p.second;
+    //    if(x<0 or y<0 or x>=w or y>=h) continue;
+    //    if(table[x][y]!='#' and !state.visit[x][y]) cnt++;
+    //    state.visit[x][y] = true;
+    //}
 
     // 倍速移動中
     if(state.wheelturn>0){
         if(valid_index(state.x+dx,state.y+dy) and table[state.x+dx][state.y+dy]!='#'){
             state.x += dx;
             state.y += dy;
+            state.touch();
             // bodyの通過
-            if(!state.visit[state.x][state.y]) cnt++;
-            if(table[state.x][state.y] == 'B') state.arm++;
-            if(table[state.x][state.y] == 'F') state.wheel++;
-            state.visit[state.x][state.y] = true;
+            //if(!state.visit[state.x][state.y]) cnt++;
+            //if(table[state.x][state.y] == 'B') state.arm++;
+            //if(table[state.x][state.y] == 'F') state.wheel++;
+            //state.visit[state.x][state.y] = true;
 
-            // 手が届く範囲
-            for(const auto& p : state.arms){
-                int x = state.x + p.first;
-                int y = state.y + p.second;
-                if(x<0 or y<0 or x>=w or y>=h) continue;
-                if(table[x][y]!='#' and !state.visit[x][y]) cnt++;
-                state.visit[x][y] = true;
-            }
+            //// 手が届く範囲
+            //for(const auto& p : state.arms){
+            //    int x = state.x + p.first;
+            //    int y = state.y + p.second;
+            //    if(x<0 or y<0 or x>=w or y>=h) continue;
+            //    if(table[x][y]!='#' and !state.visit[x][y]) cnt++;
+            //    state.visit[x][y] = true;
+            //}
         }
         state.wheelturn--;
     }
     state.value -= cnt;
 }/*}}}*/
 
-vector<vector<P>> dist;
-void dfs(int x,int y,int px,int py,int d){
-    if(dist[x][y].first!=INF) return;
-    if(table[x][y] == '#') return;
-    dist[x][y] = P(d,pii(px,py));
-
-    rep(i,4){
-        int nx = x + dx[i];
-        int ny = y + dy[i];
-        if(!valid_index(nx,ny)) continue;
-        dfs(nx,ny,x,y,d+1);
-    }
-}
 
 vector<vector<P>> bfs(int x,int y){
     vector<vector<P>> dist(w,vector<P>(h,P(INF,pii(-1,-1))));
@@ -174,23 +89,37 @@ vector<vector<P>> bfs(int x,int y){
     return dist;
 }
 
-void traverse(State& now){
-    pii nowp = pii(now.x,now.y);
-    rep(i,4){
-        int nx = now.x + dx[i];
-        int ny = now.y + dy[i];
-        if(!valid_index(nx,ny)) continue;
-        if(now.visit[nx][ny]) continue;
-        if(table[nx][ny] == '#') continue;
-
-        if(dist[nx][ny].second == nowp){
-            cout << direction(nowp,pii(nx,ny));
-            update_move(now,dx[i],dy[i]);
-            traverse(now);
-            cout << direction(pii(nx,ny),nowp);
-            update_move(now,-dx[i],-dy[i]);
+// 最寄りの腕を手に入れる
+// limitを指定した場合は距離がlimitを超えた場合は何もしない
+int get_arm(State& now,int limit = INF){
+    auto dist = bfs(now.x,now.y);
+    int cost = INF;
+    pii pos;
+    rep(i,w) rep(j,h){
+        if(table[i][j] == 'B' && dist[i][j].first < cost){
+            cost = dist[i][j].first;
+            pos = pii(i,j);
         }
     }
+    if(cost == INF) return -1;
+
+
+    if(cost > limit) return -1;
+
+    vector<char> vc;
+    while(pos != pii(now.x,now.y)){
+        pii pre = dist[pos.first][pos.second].second;
+        vc.emplace_back(direction(pre,pos));
+        pos = pre;
+    }
+
+    for(int i=vc.size()-1;i>=0;i--){
+        cout << vc[i];
+        int idx = char_to_index(vc[i]);
+        update_move(now,dx[idx],dy[idx]);
+    }
+    table[now.x][now.y] = '.';
+    return 0;
 }
 
 int main(){
@@ -198,46 +127,53 @@ int main(){
     cin >> tmp >> offset_x >> offset_y;
     cin >> tmp >> w >> h;
     cin >> tmp >> sx >> sy;
-
     table = vector<vector<char>>(w,vector<char>(h));
     {
         vector<string> tmp(h);
         rep(i,h) cin >> tmp[i];
-
         rep(i,h) rep(j,w) table[j][h-1-i] = tmp[i][j];
     }
 
-    deque<State> deq;
 
-    State initial;
-    initial.init();
-    initial.x = sx;
-    initial.y = sy;
-    initial.arms = vector<pii>{pii(1,1),pii(1,0),pii(1,-1)};
-    initial.visit[sx][sy] = true;
-    for(pii p : initial.arms){
-        int x = initial.x + p.first;
-        int y = initial.y + p.second;
+
+    State now;
+    now.init();
+    now.x = sx;
+    now.y = sy;
+    now.arms = vector<pii>{pii(1,1),pii(1,0),pii(1,-1)};
+    now.visit[sx][sy] = true;
+
+    for(pii p : now.arms){
+        int x = now.x + p.first;
+        int y = now.y + p.second;
         if(valid_index(x,y) && table[x][y] != '#'){
-            initial.visit[x][y] = true;
+            now.visit[x][y] = true;
         }
     }
-    initial.value = 0;
+    now.value = 0;
     for(int i=0;i<w;i++){
         for(int j=0;j<h;j++){
-            if(table[i][j] != '#' && !initial.visit[i][j]) initial.value++;
+            if(table[i][j] != '#' && !now.visit[i][j]) now.value++;
         }
     }
-    deq.push_front(initial);
 
-    State now(initial);
+
+    // 近い場合は腕を取りに行く
+    while(get_arm(now,20) == 0){
+        // 腕を生やす
+        pii arm = now.add_arm();
+        //cerr << "check1: " << now.x << " " << now.y << endl;
+        cout << "B" << arm;
+    }
+
+
+    // ビームサーチ
+    deque<State> deq;
+    deq.push_front(now);
 
     int turn = 0;
     // あんまりturn数を多くしても無駄が生じるのでパラメータを上手く定める関数が必要
     while(++turn < h*w*2){
-        /* cerr << endl; */
-        /* cerr << "turn = " << turn << endl; */
-        /* cerr << table[now.x][now.y] << endl; */
         deq.clear();
         deq.push_back(now);
         for(int i=0;i<DEPTH;i++){
@@ -283,7 +219,6 @@ int main(){
             while(next_deq.size() > BEAM){
                 next_deq.pop_back();
             }
-            /* cerr << "depth " << i << " -> " << next_deq.size() << endl; */
             deq = move(next_deq);
         }
 
@@ -357,14 +292,16 @@ int main(){
             }
         }
 
-        /* cerr << "from " << endl; */
-        /* print(now); */
-
-        /* cerr << "to " << endl; */
-        /* cerr << p << endl; */
-
         vector<char> vc;
-        if(p.first == -1 and p.second == -1) return 0;
+        if(p.first == -1 and p.second == -1){
+            cerr << endl << "Error" << endl;
+            int err = 0;
+            rep(i,w) rep(j,h){
+                if(table[i][j]!='#' && !now.visit[i][j]) err++;
+            }
+            cerr << err << endl;
+            return 0;
+        }
         while(p != dist[p.first][p.second].second){
             pii pa = dist[p.first][p.second].second;
             if(p == pa) break;
@@ -373,7 +310,6 @@ int main(){
             assert( p != pii(-1,-1));
         }
 
-        /* cout << endl << "----" << endl; */
         for(int i=vc.size()-1;i>=0;i--){
             cout << vc[i];
             if(vc[i] == 'D') update_move(now,1,0);
@@ -381,8 +317,11 @@ int main(){
             if(vc[i] == 'A') update_move(now,-1,0);
             if(vc[i] == 'S') update_move(now,0,-1);
         }
-        /* cout << endl << "----" << endl; */
-        /* cerr << "result "; */
-        /* print(now); */
     }
+
+    int err = 0;
+    rep(i,w) rep(j,h){
+        if(table[i][j]!='#' && !now.visit[i][j]) err++;
+    }
+    cerr << err << endl;
 }
---
+++ chakku/solve/poi.hpp
@@ -0,0 +1,165 @@
+#ifndef __POI__
+#define __POI__
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+template<typename T> ostream& operator<<(ostream& os,const vector<T>& vec){ os << "["; for(const auto& v : vec){ os << v << ","; } os << "]"; return os; }
+template<typename T,typename U> ostream& operator<<(ostream& os, const map<T,U>& mp){ os << "{"; for(auto& p : mp){ os << p << ","; } os << "}"; return os; }
+template<typename T,typename U> ostream& operator<<(ostream& os,const pair<T,U>& p){ os << "(" << p.first << ","<< p.second <<")"; return os; }
+template<typename T> ostream& operator<<(ostream& os,const set<T>& st){ os<<"{"; for(T v:st) os<<v<<","; os <<"}"; return os; }
+
+#define rep(i,n) for(int i=0;i<(int)(n);i++)
+#define all(v) (v).begin(),(v).end()
+
+using pii = std::pair<int,int>;
+using P = std::pair<int,pii>;
+
+
+const int dx[]={1,0,-1,0} ,dy[] = {0,1,0,-1};
+const char word[] = {'D','W','A','S'};
+constexpr int INF = numeric_limits<int>::max()/2;
+
+
+int offset_x,offset_y;
+int w,h;
+int sx,sy;
+vector<vector<char>> table;
+
+constexpr int BEAM = 8;
+constexpr int DEPTH = 30;
+
+
+char direction(const pii& p1,const pii& p2){
+    for(int i=0;i<4;i++){
+        if(p1.first + dx[i] == p2.first and p1.second + dy[i] == p2.second){
+            return word[i];
+        }
+    }
+    assert(false);
+}
+
+int char_to_index(char c){
+    for(int i=0;i<4;i++){
+        if(word[i] == c){
+            return i;
+        }
+    }
+    assert(false);
+}
+
+bool valid_index(int x,int y){ return !(x<0 or y<0 or x>=w or y>=h); }
+
+struct Action{
+    char action;
+    int x,y;
+    Action(char c): action(c){}
+    Action(char c,int xx,int yy) : action(c) , x(xx) , y(yy){}
+};
+ostream& operator<<(ostream& os, const Action& action){
+    os << "Action(" << action.action<< ")";
+    return os;
+}
+
+struct State{
+public:
+    int x,y;
+    int value;
+    int wheel=0;
+    /* int drill=0; */
+    int arm=0;
+    int wheelturn=0;
+    /* int drillturn=0; */
+    vector<vector<bool>> visit;
+    vector<pii> arms;
+    vector<Action> actions;
+
+    int eval()const{
+        return value + actions.size() - arms.size();
+    }
+
+    bool operator<(const State& st)const{
+        return eval() < st.eval();
+    }
+    void init(){
+        visit.assign(w,vector<bool>(h,false));
+    }
+    pii add_arm(){
+        if(arms.size() == 3) arms.push_back(pii(-1,0));
+        else if(arms.size() == 4) arms.emplace_back(pii(-1,1));
+        else if(arms.size() == 5) arms.emplace_back(pii(-1,-1));
+        else if(arms.size() == 6) arms.emplace_back(pii(0,1));
+        else if(arms.size() == 7) arms.emplace_back(pii(0,-1));
+        else{
+            arms.emplace_back(arms.size()-6,0);
+        }
+        return arms[arms.size()-1];
+    }
+
+    // 今いる場所から触れる領域を計算する
+    void touch(){
+        int cnt = 0;
+        if(!visit[x][y]) cnt++;
+        visit[x][y] = true;
+        if(table[x][y]=='B'){
+            table[x][y] = '.';
+            arm++;
+        }
+        if(table[x][y]=='F'){
+            table[x][y] = '.';
+            wheel++;
+        }
+
+        // 手が届く範囲を計算
+        for(const auto& p : arms){
+            int nx = x + p.first;
+            int ny = y + p.second;
+            if(!valid_index(nx,ny)) continue;
+            if(table[nx][ny] == '#') continue;
+            if(p.first==0||p.second==0){ // 上下左右に伸びている
+                if(p.first==0){
+                    int sy = min(y,ny);
+                    int gy = max(y,ny);
+                    bool ok = true;
+                    for(int yy=sy;yy<=gy;yy++){
+                        if(table[x][yy] == '#'){
+                            ok = false; break;
+                        }
+                    }
+                    if(ok){
+                        for(int yy=sy;yy<=gy;yy++){
+                            if(!visit[x][yy]) cnt++;
+                            visit[x][yy] = true;
+                        }
+                    }
+                }else if(p.second==0){
+                    int sx = min(x,nx);
+                    int gx = max(x,nx);
+                    bool ok = true;
+                    for(int xx=sx;xx<=gx;xx++){
+                        if(table[xx][y]=='#'){
+                            ok = false; break;
+                        }
+                    }
+                    if(ok){
+                        for(int xx=sx;xx<=gx;xx++){
+                            if(!visit[xx][y]) cnt++;
+                            visit[xx][y] = true;
+                        }
+                    }
+                }
+            }else{  // 斜めにある
+                if(!visit[nx][ny]) cnt++;
+                visit[nx][ny] = true;
+            }
+        }
+        value -= cnt;
+    }
+};
+
+void print(State& s){
+    cerr << "(" << s.x << "," << s.y << ") , " << "value = " << s.value << " actions = " << s.actions << endl;
+}
+
+#endif
